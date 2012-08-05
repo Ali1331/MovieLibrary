ebc6ac92b8583c559d6ccedfed357618e5c0785f
Ali <ali.neowind@gmail.com>
2012-08-05 01:09:13 +0100

    Initial

---
+++ src/movielibrary/MovieLibrary.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movielibrary;
+
+/**
+ *
+ * @author Ali
+ */
+public class MovieLibrary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
